cb9dffbfd80c1b27ec014c759bc8ba88d1926565
saoufi sami <saoufi-sami@hotmail.fr>
2023-11-16 20:43:17 +0100

    test

--- backend/app/Http/Controllers/ContactController.php
+++ backend/app/Http/Controllers/ContactController.php
@@ -6,5 +6,29 @@ use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
-    //
+    public function list()
+    {
+        // Logique pour afficher toutes les adresses
+    }
+
+    public function show($id)
+    {
+        // Logique pour afficher une adresse spécifique
+    }
+
+    public function create(Request $request)
+    {
+        // Logique pour créer une nouvelle adresse
+    }
+
+    public function update(Request $request, $id)
+    {
+        // Logique pour mettre à jour une adresses
+    }
+
+    public function delete($id)
+    {
+        // Logique pour supprimer une adresse
+    }
 }
+
