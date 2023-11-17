28b0636b033f43ed1933395dde6d7a6d5d3142d3
saoufi sami <saoufi-sami@hotmail.fr>
2023-11-17 17:12:40 +0100

    test

--- backend/app/Http/Controllers/ContactController.php
+++ backend/app/Http/Controllers/ContactController.php
@@ -28,7 +28,7 @@ class ContactController extends Controller
 
     public function delete($id)
     {
-        // Logique pour supprimer une adresses
+        // Logique pour supprimer une adressess
     }
 }
 
