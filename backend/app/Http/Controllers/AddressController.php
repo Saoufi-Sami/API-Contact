839426a0f063423d55c70d06e7343e1d53ece1de
saoufi sami <saoufi-sami@hotmail.fr>
2023-11-21 14:19:29 +0100

    adress Controller debut

--- backend/app/Http/Controllers/AddressController.php
+++ backend/app/Http/Controllers/AddressController.php
@@ -6,5 +6,31 @@ use Illuminate\Http\Request;
 
 class AddressController extends Controller
 {
-    //
-}
+    
+        public function list()
+        {
+           
+        }
+    
+        public function show($id)
+        {
+            
+        }
+    
+        public function create(Request $request)
+        {
+           
+        }
+    
+        public function update(Request $request, $id)
+        {
+            
+        }
+    
+        public function delete($id)
+        {
+           
+        }
+    }
+    
+
--- backend/app/Http/Controllers/ContactController.php
+++ backend/app/Http/Controllers/ContactController.php
@@ -8,27 +8,27 @@ class ContactController extends Controller
 {
     public function list()
     {
-        // Logique pour afficher toutes les adresses
+       
     }
 
     public function show($id)
     {
-        // Logique pour afficher une adresse spécifique
+        
     }
 
     public function create(Request $request)
     {
-        // Logique pour créer une nouvelle adresse
+       
     }
 
     public function update(Request $request, $id)
     {
-        // Logique pour mettre à jour une adresses
+        
     }
 
     public function delete($id)
     {
-        // Logique pour supprimer une adresses
+       
     }
 }
 
