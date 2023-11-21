ed9450ca67b048b5625906b71fb67f8e69d0f58b
saoufi sami <saoufi-sami@hotmail.fr>
2023-11-21 15:51:22 +0100

    Controller Contact et adresse ok

--- backend/app/Http/Controllers/AddressController.php
+++ backend/app/Http/Controllers/AddressController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Address;
 use Illuminate\Http\Request;
 
 class AddressController extends Controller
@@ -9,27 +10,92 @@ class AddressController extends Controller
     
         public function list()
         {
-           
+           // Récupérer toutes les adresses depuis la base de données
+    $addresses = Address::all();
+
+    // Renvoyer les adresses en tant que réponse JSON
+    return response()->json($addresses);
         }
+
+
+
+
+
+
     
         public function show($id)
         {
-            
+             // Récupérer l'adresse depuis la base de données en fonction de l'ID
+    $address = Address::findOrFail($id);
+
+    // Renvoyer l'adresse en tant que réponse JSON
+    return response()->json($address);
         }
+
+
+
+
+
+
+
+
+
+
     
         public function create(Request $request)
         {
-           
+            // Valider les données de la requête
+            $request->validate([
+               
+                'ville' => 'required',
+         
+            ]);
+        
+            // Créer une nouvelle adresse dans la base de données
+            $address = Address::create($request->all());
+        
+            // Renvoyer les détails de la nouvelle adresse en tant que réponse JSON
+            return response()->json($address, 201);
         }
+
+
+
+
+
+
+
+
+
+
+
     
         public function update(Request $request, $id)
         {
-            
+               // Valider les données de la requête
+    $request->validate([
+        'ville' => 'required',
+    ]);
+
+    // Récupérer l'adresse depuis la base de données en fonction de l'ID
+    $address = Address::findOrFail($id);
+
+    // Mettre à jour les détails de l'adresse
+    $address->update($request->all());
+
+    // Renvoyer les détails de l'adresse mise à jour en tant que réponse JSON
+    return response()->json($address);
         }
     
         public function delete($id)
         {
-           
+            // Récupérer l'adresse depuis la base de données en fonction de l'ID
+    $address = Address::findOrFail($id);
+
+    // Supprimer l'adresse
+    $address->delete();
+
+    // Renvoyer une réponse indiquant le succès de l'opération
+    return response()->json(null, 204);
         }
     }
     
--- backend/app/Http/Controllers/ContactController.php
+++ backend/app/Http/Controllers/ContactController.php
@@ -2,33 +2,53 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Contact;
 use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
     public function list()
     {
-       
+        $contacts = Contact::all();
+        return response()->json($contacts);
     }
 
     public function show($id)
     {
-        
+        $contact = Contact::findOrFail($id);
+        return response()->json($contact);
     }
 
     public function create(Request $request)
     {
-       
+        $request->validate([
+            'nom' => 'required',
+            'prenom' => 'required',
+        ]);
+
+        $contact = Contact::create($request->all());
+        return response()->json($contact, 201);
     }
 
     public function update(Request $request, $id)
     {
-        
+        $request->validate([
+            'nom' => 'required',
+            'prenom' => 'required',
+        ]);
+
+        $contact = Contact::findOrFail($id);
+        $contact->update($request->all());
+
+        return response()->json($contact);
     }
 
     public function delete($id)
     {
-       
+        $contact = Contact::findOrFail($id);
+        $contact->delete();
+
+        return response()->json(null, 204);
     }
 }
 
